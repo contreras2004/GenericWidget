8e3b0336503acbec82a2f7caf3a98461cdfcbe25
Matias Contreras <contreras2004@gmail.com>
2021-01-08 12:03:56 -0300

    First commit

---
+++ Example/Pods/Target Support Files/GenericWidget/GenericWidget-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GenericWidget : NSObject
+@end
+@implementation PodsDummy_GenericWidget
+@end
---
+++ Example/Pods/Target Support Files/Pods-GenericWidget_Example/Pods-GenericWidget_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GenericWidget_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GenericWidget_Example
+@end
